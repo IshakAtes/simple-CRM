84c2961449c10c5cc01219a8f2742d4daa34a01c
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-04-23 14:13:36 +0200

    firestore implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { initializeApp } from "firebase/app";
+import { Observable } from 'rxjs';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAd0uPXiizGQhtdSWPkZHHXfr8KQcADhEI",
+  authDomain: "simplecrm-b3a59.firebaseapp.com",
+  projectId: "simplecrm-b3a59",
+  storageBucket: "simplecrm-b3a59.appspot.com",
+  messagingSenderId: "737703168763",
+  appId: "1:737703168763:web:3e92a6c5bb7ad11af53e5e"
+};
+
+
 
 @Component({
   selector: 'app-root',
@@ -12,6 +26,18 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'simpleCRM';
+  firestore: Firestore = inject(Firestore);
+  app = initializeApp(firebaseConfig);
+  items$: Observable<any[]>;
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'users');
+    this.items$ = collectionData(itemCollection);
+    console.log('update', this.items$.subscribe((g) => {
+      console.log('GameUpdate', g);
+    }));
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyAd0uPXiizGQhtdSWPkZHHXfr8KQcADhEI",
+      authDomain: "simplecrm-b3a59.firebaseapp.com",
+      projectId: "simplecrm-b3a59",
+      storageBucket: "simplecrm-b3a59.appspot.com",
+      messagingSenderId: "737703168763",
+      appId: "1:737703168763:web:3e92a6c5bb7ad11af53e5e"
+    }))),
+    importProvidersFrom(provideFirestore(() => getFirestore()))
+  ]
 };
